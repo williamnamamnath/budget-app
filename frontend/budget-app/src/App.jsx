099cdc33673667ba9a670509952e9150509aca80
William Nam-Amnath <william.nam-amnath@mail.mcgill.ca>
2025-07-06 12:50:11 -0400

    Setup main pages for app

--- frontend/budget-app/src/App.jsx
+++ frontend/budget-app/src/App.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import Login from './pages/authentication/Login';
+import Signup from './pages/authentication/Signup';
+import Home from './pages/dashboard/Home';
+import Income from './pages/dashboard/Income';
+import Expenses from './pages/dashboard/Expenses';
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/home" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/dashboard" exact element={<Expenses />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
 export default App;
+
+const Root = () => {
+  
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  return isAuthenticated ? (
+    <Navigate to="/home" />
+  ) : (
+    <Navigate to="/login" />
+  )
+}
