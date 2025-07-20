b6530ad8bdb21a1d312d80358bb9ff5ed95e447a
William Nam-Amnath <william.nam-amnath@mail.mcgill.ca>
2025-07-20 12:23:39 -0400

    Set up connection to MongoDB database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const connectDB = require('./config/db');
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(cors({
 );
 
 app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
